deb01c7d64e8b73eb021463f1e320384e55c5aa9
Sajjad Hossain <155253869+Sajjad177@users.noreply.github.com>
2025-08-22 05:25:48 +0600

    feat(facility): implement create facility endpoint with image and video upload

--- src/routes/facility.router.ts
+++ src/routes/facility.router.ts
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import facilityController from "../controllers/facility.controller";
+import { isOrganization, protect } from "../middlewares/auth.middleware";
+import { upload } from "../middlewares/multer.middleware";
 
 const router = Router();
+
+router.post(
+  "/create",
+  protect,
+  isOrganization,
+  upload.fields([
+    { name: "image", maxCount: 5 },
+    { name: "video", maxCount: 1 },
+  ]),
+  facilityController.createFacility
+);
 
 export const facilityRouter = router;
