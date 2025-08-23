4a79fa84304aa432f8f1bae0bc3fa7ed87b26616
zihadulislam2 <zihadul708@gmail.com>
2025-08-24 01:53:51 +0600

    change env for node mailer

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,12 +1,12 @@
-import { ErrorRequestHandler } from 'express';
+import { ErrorRequestHandler } from 'express'
 // import config from '../app/config';
-import { ZodError } from 'zod';
-import { TErrorSources } from '../interface/error';
-import handleZodError from '../errors/handleZodError';
-import handleValidationError from '../errors/handleValidationError';
-import handleCastError from '../errors/handleCastErrror';
-import handleDuplicateError from '../errors/handleDuplicateKeyError';
-import AppError from '../errors/AppError';
+import { ZodError } from 'zod'
+import { TErrorSources } from '../interface/error'
+import handleZodError from '../errors/handleZodError'
+import handleValidationError from '../errors/handleValidationError'
+import handleCastError from '../errors/handleCastErrror'
+import handleDuplicateError from '../errors/handleDuplicateKeyError'
+import AppError from '../errors/AppError'
 // import { TErrorSources } from '../app/interface/error';
 // import handleZodError from '../app/errors/handleZodError';
 // import handleValidationError from '../app/errors/handleValidationError';
@@ -15,74 +15,73 @@ import AppError from '../errors/AppError';
 // import AppError from '../app/errors/AppError';
 
 const setErrorDetails = (simplifiedError: {
-  statusCode: number;
-  message: string;
-  errorSources: TErrorSources;
+  statusCode: number
+  message: string
+  errorSources: TErrorSources
 }) => {
   return {
     statusCode: simplifiedError.statusCode,
     message: simplifiedError.message,
     errorSources: simplifiedError.errorSources,
-  };
-};
+  }
+}
 
 // 14-2,3
 
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 export const globalErrorHandler: ErrorRequestHandler = (
   err,
   req,
   res,
   // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  next,
+  next
 ) => {
   // setting default values
-  let statusCode = 500;
-  let message = 'An unexpected error occurred';
+  let statusCode = 500
+  let message = 'An unexpected error occurred'
 
   let errorSources: TErrorSources = [
     {
       path: '',
       message: 'Something went wrong',
     },
-  ];
+  ]
 
   if (err instanceof ZodError) {
-    const simplifiedError = handleZodError(err);
-    ({ statusCode, message, errorSources } = setErrorDetails(simplifiedError));
+    const simplifiedError = handleZodError(err)
+    ;({ statusCode, message, errorSources } = setErrorDetails(simplifiedError))
   } else if (err?.name === 'ValidationError') {
     // Mongoose validation error
-    const simplifiedError = handleValidationError(err);
-    ({ statusCode, message, errorSources } = setErrorDetails(simplifiedError));
+    const simplifiedError = handleValidationError(err)
+    ;({ statusCode, message, errorSources } = setErrorDetails(simplifiedError))
   } else if (err?.name === 'CastError') {
     // Mongoose cast error
-    const simplifiedError = handleCastError(err);
-    ({ statusCode, message, errorSources } = setErrorDetails(simplifiedError));
+    const simplifiedError = handleCastError(err)
+    ;({ statusCode, message, errorSources } = setErrorDetails(simplifiedError))
   } else if (err?.code === 11000) {
     // duplicate key error mongoose
-    const simplifiedError = handleDuplicateError(err);
-    ({ statusCode, message, errorSources } = setErrorDetails(simplifiedError));
+    const simplifiedError = handleDuplicateError(err)
+    ;({ statusCode, message, errorSources } = setErrorDetails(simplifiedError))
   } else if (err instanceof AppError) {
     // APPError handle custom
-    statusCode = err?.statusCode;
-    message = err?.message;
+    statusCode = err?.statusCode
+    message = err?.message
     errorSources = [
       {
         path: '',
         message: err.message,
       },
-    ];
+    ]
   } else if (err instanceof Error) {
     //default error pattern
-    message = err?.message;
+    message = err?.message
     errorSources = [
       {
         path: '',
         message: err.message,
       },
-    ];
+    ]
   }
 
   return res.status(statusCode).json({
@@ -91,5 +90,5 @@ export const globalErrorHandler: ErrorRequestHandler = (
     errorSources,
     // error: err,
     // stack: config.NODE_ENV === 'production' ? null : err.stack,
-  });
-};
+  })
+}
--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -2,14 +2,14 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: 'smtp.gmail.com',
     port: 587,
     secure: false,
     auth: {
-      user: "tahsin.bdcalling@gmail.com",
-      pass: "lcnt cxiw pcui vikv",
+      user: process.env.APP_USER,
+      pass: process.env.APP_PASS,
     },
-  });
+  })
   await transporter.sendMail({
     from: "nm.bdcalling@gmail.com", // sender address
     to,
