2ddfd30b7859e6d36c7c082876697927a2e1bd33
zihadulislam2 <zihadul708@gmail.com>
2025-09-01 02:15:10 +0600

    fix error

--- dist/Payment/payment.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_middleware_1 = require("../middlewares/auth.middleware");
-const payment_controller_1 = require("./payment.controller");
-const router = (0, express_1.Router)();
-router.post('/pay', auth_middleware_1.protect, payment_controller_1.createPayment);
-const payment = router;
-exports.default = payment;
